b012adcbf90521f0880af8dbd4013fd54778b065
Igor Nikolaev <igorabcp@gmail.com>
2023-07-12 22:10:03 +0400

    Init commit

---
+++ Lesson6/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Lesson6.Models;
+
+namespace Lesson6.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var model = new HelloWorldModel
+        {
+            Text = "Ivan!"
+        };
+        ViewData["Message"] = "Some message";
+        ViewBag.text_another = "Some another text";
+        return View(model);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Lesson6/Models/HelloWorldModel.cs
@@ -0,0 +1,6 @@
+namespace Lesson6.Models;
+
+public class HelloWorldModel
+{
+    public string Text { get; set; }
+}
